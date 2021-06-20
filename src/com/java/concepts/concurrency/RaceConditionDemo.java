21613ca9f0959971de863d2c643dd8b27883ccab
Apurv Upreti <vontuguna@gmail.com>
2021-06-20 12:20:44 +0530

    Race Condition

---
+++ src/com/java/concepts/concurrency/RaceConditionDemo.java
@@ -0,0 +1,41 @@
+package com.java.concepts.concurrency;
+
+public class RaceConditionDemo {
+
+    public static void main(String[] args){
+        BankAccount task = new BankAccount();
+        task.setBalance(100);
+
+        Thread john = new Thread(task);
+        Thread anitha = new Thread(task);
+
+        john.setName("John");
+        anitha.setName("Anitha");
+        john.start();
+        anitha.start();
+    }
+
+}
+
+class BankAccount implements Runnable{
+    private int balance;
+    public void setBalance(int balance){
+        this.balance = balance;
+    }
+    
+    public void run(){
+        makeWithdrawel(75);
+        if(balance<0){
+            System.out.println("Money overdrawn.");
+        }
+    }
+
+    private void makeWithdrawel(int amount) {
+        if(amount>balance){
+            System.out.println("Not enough balance");
+        }else{
+            System.out.println(Thread.currentThread().getName() + " is withdrawing "+ amount);
+            balance-=amount;
+        }
+    }
+}
